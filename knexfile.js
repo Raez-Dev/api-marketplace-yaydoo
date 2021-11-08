9e7cdaa8d93619e60d478ee59f0547b89c404451
Raez-Dev <cr.unfv@gmail.com>
2021-11-08 09:05:23 -0500

    db keys

--- knexfile.js
+++ knexfile.js
@@ -1,27 +1,29 @@
 // Update with your config settings.
 const { knexSnakeCaseMappers } = require('objection');
+const config = require('./src/config');
 
 module.exports = {
-
   development: {
     client: 'postgresql',
     connection: {
-      database: 'marketplace-yaydoo',
-      user: 'postgres',
-      password: '123'
+      host: config.dbHost,
+      port: config.port,
+      database: config.dbDatabase,
+      user: config.dbUser,
+      password: config.password,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
       tableName: 'knex_migrations',
       directory: './src/db/migrations',
     },
-    seeds: {                // Initial data
+    seeds: {
+      // Initial data
       directory: './src/db/seeds',
     },
     ...knexSnakeCaseMappers, // To format
   },
-
 };
--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,11 @@ require('dotenv').config();
 const config = {
   env: process.env.NODE_ENV,
   keyJWT: process.env.KEY_JWT,
-  dbFilename: process.env.DB_FILENAME || '',
-  dbTestFilename: process.env.DB_TEST_FILENAME || '',
+  dbHost : process.env.DB_HOST,
+  dbDatabase : process.env.DB_DATABASE,
+  dbUser : process.env.DB_USER,
+  dbPort : process.env.DB_PORT,
+  dbPassword : process.env.DB_PASSWORD,
   port: process.env.APP_PORT ? parseInt(process.env.APP_PORT, 10) : 5000,
 };
 
