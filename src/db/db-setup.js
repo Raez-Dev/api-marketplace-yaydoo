369936368351ed7349b30b5ed80dfa434b06c405
Raez-Dev <cr.unfv@gmail.com>
2021-11-10 16:22:49 -0500

    s upd

--- src/db/db-setup.js
+++ src/db/db-setup.js
@@ -1,9 +1,10 @@
 const knex = require('knex');
 const knexfile = require('../../knexfile');
 const { Model } = require('objection');
+const config = require('../config/');
 
 function setupDb() {
-    const db = knex(knexfile.development);
+    const db = knex(knexfile[config.env]);
     Model.knex(db);
 }
 
